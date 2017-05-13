39b955a1887c2e06cf5cc00d826cc6206c830557
zhaokejian <zhaokejian@zju.edu.cn>
2017-05-12 23:54:46 -0700

    init for SenImgVis-meanjs

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -1,7 +1,7 @@
 (function (window) {
   'use strict';
 
-  var applicationModuleName = 'mean';
+  var applicationModuleName = 'SenImgVis';
 
   var service = {
     applicationEnvironment: window.env,
--- modules/core/client/core.client.module.js
+++ modules/core/client/core.client.module.js
@@ -3,6 +3,6 @@
 
   app.registerModule('core');
   app.registerModule('core.routes', ['ui.router']);
-  app.registerModule('core.admin', ['core']);
-  app.registerModule('core.admin.routes', ['ui.router']);
+  // app.registerModule('core.admin', ['core']);
+  // app.registerModule('core.admin.routes', ['ui.router']);
 }(ApplicationConfiguration));
